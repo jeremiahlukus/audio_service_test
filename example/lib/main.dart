b43a3493aaa65681a8c2732882b916d9b9337e9d
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-05-04 23:14:44 -0400

    Jeremiah-Parrack |

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -772,13 +772,46 @@ class MediaLibrary {
   };
 }
 
+class MediaLibrary2 {
+  static const albumsRootId = 'albums';
+
+  final items = <String, List<MediaItem>>{
+    AudioService.browsableRootId: const [
+      MediaItem(
+        id: albumsRootId,
+        album: "",
+        title: "Albums",
+        playable: false,
+      ),
+    ],
+    albumsRootId: [
+      MediaItem(
+        id: 'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
+        album: "Science Friday",
+        title: "A Salute To Head-Scratching Science",
+        artist: "Science Friday and WNYC Studios",
+        duration: const Duration(milliseconds: 5739820),
+        artUri: Uri.parse('https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg'),
+      ),
+      MediaItem(
+        id: 'https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3',
+        album: "Science Friday",
+        title: "From Cat Rheology To Operatic Incompetence",
+        artist: "Science Friday and WNYC Studios",
+        duration: const Duration(milliseconds: 2856950),
+        artUri: Uri.parse('https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg'),
+      ),
+    ],
+  };
+}
+
 /// This task defines logic for speaking a sequence of numbers using
 /// text-to-speech.
 
 class TextPlayerHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
   // ignore: close_sinks
   final BehaviorSubject<List<MediaItem>> _recentSubject = BehaviorSubject<List<MediaItem>>();
-  final _mediaLibrary = MediaLibrary();
+  final _mediaLibrary = MediaLibrary2();
   final _player = AudioPlayer();
 
   int? get index => _player.currentIndex;
